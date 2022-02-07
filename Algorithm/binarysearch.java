6ce11601d75a2c22b447e8986465eceee349cd85
saifrahmn <saifrahmn78@gmail.com>
2022-02-07 22:55:44 +0530

    changes

--- Algorithm/binarysearch.java
+++ Algorithm/binarysearch.java
@@ -1,1 +1,23 @@
-//the code is yet to be written
+
+public class binarysearch{
+    public static int search(int[] arr,int tar){
+        int len= arr.length;
+        for(int i=0;i<len;i++){
+            int mid=(len/2);
+            if(arr[mid]>tar){
+                len = mid;
+            } 
+             else if(arr[mid]<tar){
+                i = mid;
+            }
+            else{
+                return mid;
+            }
+        } 
+        return 0;
+    }
+    public static void main(String args[]){
+        int arr[]={2,5,8,12,16,20,38,56,72,91},target=12;
+        System.out.println("the output of the search is" + search(arr,target));
+    }
+}
