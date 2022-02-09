db692f4225af3513e43f0389a67fba1e706194df
saifrahmn <saifrahmn78@gmail.com>
2022-02-09 19:54:22 +0530

    new file

---
+++ Algorithm/longestsubstring.java
@@ -0,0 +1,11 @@
+//leetcode ques no3
+public class longestsubstring {
+    public int lengthOfLongestSubstring(String s) {
+        String st=new String();
+        return 2;
+    }
+    public static void main(String[] args){
+        String[] s={"a","b","c","a","b","c","c","b","b"};
+        System.out.println(lengthOfLongestSubstring(String s));
+    }
+}
