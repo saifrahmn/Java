986b35f838c71469e1540c46d274df9d02f64b8b
saifrahmn <saifrahmn78@gmail.com>
2022-02-11 19:21:36 +0530

    update

--- Algorithm/binarysearch.java
+++ Algorithm/binarysearch.java
@@ -20,6 +20,18 @@ public class binarysearch{
         return -1;
        
     }
+    /*
+    public int search(int[] nums, int target) {
+        int pivot, left = 0, right = nums.length - 1;
+        while (left <= right) {
+          pivot = left + (right - left) / 2;
+          if (nums[pivot] == target) return pivot;
+          if (target < nums[pivot]) right = pivot - 1;
+          else left = pivot + 1;
+        }
+        return -1;
+      }
+    */
     public static void main(String args[]){
         int arr[]={2,5,8,12,16,20,38,56,72,91},target=38;
         System.out.println("the output of the search is" + search(arr,target));
