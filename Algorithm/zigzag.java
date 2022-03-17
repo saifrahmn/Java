9ed8360be15020914fb20c6b9ffdc5ac779150fb
saifrahmn <saifrahmn78@gmail.com>
2022-03-17 22:42:01 +0530

    create zigzag

---
+++ Algorithm/zigzag.java
@@ -0,0 +1,10 @@
+public class zigzag {
+    public static String solution(String s,int rows){
+        return -1;
+    }
+    public static void main(){
+        String s="PAYPALISHIRING";
+        int rows=3;
+        System.out.println(solution(s,rows));
+    }
+}
