14a518fbd243d12cc3bfa18867bc0dcce7a01566
saifrahmn <saifrahmn78@gmail.com>
2022-03-17 22:49:34 +0530

    update

--- Algorithm/zigzag.java
+++ Algorithm/zigzag.java
@@ -1,5 +1,12 @@
 public class zigzag {
     public static String solution(String s,int rows){
+        String arr[];
+        for(int i=0;i<=(s.length()-1);i++){
+            for(int j=0;j<rows;j++){
+                arr[j]=s.charAt(i);
+                
+            }
+        }
         return -1;
     }
     public static void main(){
@@ -7,4 +14,4 @@ public class zigzag {
         int rows=3;
         System.out.println(solution(s,rows));
     }
-}
+}    
